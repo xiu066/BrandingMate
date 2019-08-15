d7ab8bf5d7d2cd63dda0f7befb9a0f1a227a43ab
Pengbo Xiu <37978767+xiu066@users.noreply.github.com>
2019-08-15 16:15:27 +1000

    Add files via upload
    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//switch between display pages
+function showAdviserHUB(){
+    $(".show").hide();
+    $("#showAdviserHUB").show();
+}
+
+function showInvestorHUB(){
+    $(".show").hide();
+    $("#showInvestorHUB").show();
+}
+
+function showLoginPage(){
+    $(".show").hide();
+    $("#showLoginPage").show();
+}
+
+//turn on/off extra configurations for White Label
+// function checkWhiteLabel(checkbox){
+//     if(checkbox.checked==true) {
+//         $("#whiteLabelSettings").show();
+//     } else {
+//         $("#whiteLabelSettings").hide();
+//     }
+// }
+
+
