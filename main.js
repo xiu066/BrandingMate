b4e9dc37c3ac06ccd5bb7620039e24696f4ebdfd
Pengbo Xiu <37978767+xiu066@users.noreply.github.com>
2019-08-15 23:51:17 +1000

    night 20190815

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+$(document).ready(function(){
 //switch between display pages
 function showAdviserHUB(){
     $(".show").hide();
@@ -13,6 +15,28 @@ function showLoginPage(){
     $(".show").hide();
     $("#showLoginPage").show();
 }
+//change 04 ofour color
+
+
+
+
+$("#ofourSelect").change(
+    function () {
+        var color = $("#ofourSelect").val();
+        console.log(color);
+        $("#showAdviserHUB").contents().find(".font-ofour").css("color",color); 
+});
+
+
+// function ofourSelect() {
+//     // var color = $("#ofourSelect").val();
+//     // 
+// 	var iFrameDOM = $("iframe#showAdviserHUB").contents();
+
+// 	iFrameDOM.find("div").css("background-color", "#fff")
+//     //document.getElementById("demo").innerHTML = "You selected: " + x;
+//   }
+
 
 //turn on/off extra configurations for White Label
 // function checkWhiteLabel(checkbox){
@@ -23,4 +47,4 @@ function showLoginPage(){
 //     }
 // }
 
-
+});
